44b912e6dc478f151e909fd2433d4e617342993c
SumitDebBarma <sumitdebbarma321@gmail.com>
2023-09-17 16:09:35 +0530

    adding increment function

--- main.js
+++ main.js
@@ -1,41 +1,45 @@
-const shop = document.getElementById('shop')
+const shop = document.getElementById("shop");
 
 const dataItem = [
-    {
-        id:1,
-        image:"images/1.png",
-        name:"Iphone 14",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-        price: 999,
-    },
-    {
-        id:2,
-        image:"images/2.png",
-        name:"Macbook air",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-        price: 1300,
-    },
-    {
-        id:3,
-        image:"images/3.png",
-        name:"canon m2",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-        price: 499,
-    },
-    {
-        id:4,
-        image:"images/4.png",
-        name:"Armani",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-        price: 500,
-    },
-]
+  {
+    id: "one",
+    image: "images/1.png",
+    name: "Iphone 14",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    price: 999,
+  },
+  {
+    id: "Two",
+    image: "images/2.png",
+    name: "Macbook air",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    price: 1300,
+  },
+  {
+    id: "three",
+    image: "images/3.png",
+    name: "canon m2",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    price: 499,
+  },
+  {
+    id: "four",
+    image: "images/4.png",
+    name: "Armani",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    price: 500,
+  },
+];
+
+const basket = [];
+
 const shopItem = () => {
-return shop.innerHTML = dataItem.map((item) => {
-    // Deconstruction
-    const {name, image, price, description} = item
-    return ` 
-    <div class="item">
+  return (shop.innerHTML = dataItem
+    .map((item) => {
+      // Deconstruction
+      const { id, name, image, price, description } = item;
+      return ` 
+    <div id = product-id-${id}  class="item">
         <img width="200px" src=${image}>
         <div class="details">
             <h3>${name}</h3>
@@ -43,14 +47,36 @@ return shop.innerHTML = dataItem.map((item) => {
             <div class="price-quantity">
                 <h2>$ ${price}</h2>
                 <div class="buttons">
-                    <i class="bi bi-dash-lg"></i>
-                    <div class="quatity">0</div>
-                    <i class="bi bi-plus-lg"></i>
+                    <i  class="bi bi-dash-lg"></i>
+                    <div id=${id} class="quatity">0</div>
+                    <i onclick= increment(${id}) class="bi bi-plus-lg"></i>
                 </div>
             </div>
         </div>
-    </div>`
-}).join("")
-    // What does this join() method do?
-}
-shopItem()
+    </div>`;
+    })
+    .join(""));
+  // What does this join() method do?
+};
+
+shopItem();
+
+// How to select card?
+const increment = (id) => {
+  let selectedItem = id;
+  let seacrh = basket.find((x) => x.id === selectedItem.id);
+
+//   why search is equal to undefined, why not false, null or other falsey value?
+
+// if search is empty, then  push new object inside array. For eg: let search;
+  if (seacrh === undefined) {
+    basket.push({
+      id: selectedItem.id,
+      item: 1,
+    });
+  } else {
+    seacrh.item += 1;
+  }
+  console.log("selectedItem", selectedItem);
+  console.log("seacrh", seacrh)
+};
